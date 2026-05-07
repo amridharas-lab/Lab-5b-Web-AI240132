8717b635fc5fef5fe184b62b917c3eba4bc637d4
amridharas-lab <amridharas@gmail.com>
2026-05-07 20:03:23 +0800

    Update register.php

--- register.php
+++ register.php
@@ -1,3 +1,22 @@
+<?php
+include 'db.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $matric = $_POST['matric'];
+    $name = $_POST['name'];
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+    $role = $_POST['role'];
+
+    $sql = "INSERT INTO users (matric, name, password, role) VALUES ('$matric', '$name', '$password', '$role')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Registration successful! <a href='login.php'>Login here</a>";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html>
 <body>
